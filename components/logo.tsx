b484b61aed87b9ce7553ba62a64d8da9566b475f
Felix <2072738187yu@gamil.com>
2025-06-08 00:02:38 +0800

    fix: svg in image

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,13 +1,10 @@
-'use client'
-
-import Image from 'next/image'
-
+import { BrandLogo } from '@/components/icons/brand-logo'
 import { Link } from '@/i18n/navigation'
 
 const Logo = () => {
   return (
     <Link href="/" className="flex items-center gap-2">
-      <Image src="/brand-logo.svg" alt="Destiny AI Logo" width={90} height={36} priority />
+      <BrandLogo width={90} height={36} />
     </Link>
   )
 }
